7ea509e271be356f722e710ecd7f049330ec5091
stist1111 <stist1111@naver.com>
2020-08-15 21:23:55 +0900

    Updates

---
+++ precache-manifest.d3704a74f1f3608bc5eeb0c8e09bc882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a70239781ddb665a6fa9f3bdb50377",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e1ab2f442d474b3f69c0",
+    "url": "/movie_app/static/css/main.85a2c950.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ab2f442d474b3f69c0",
+    "url": "/movie_app/static/js/main.0c5a7f5d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
